f50ee65c7bdfbd845773b3df702ea83200d5152b
appkit-framework <admin@appkit.biz>
2025-12-13 00:51:11 +0100

    No need to store _servicePrefix in extraParameters

--- src/RouteCollector.php
+++ src/RouteCollector.php
@@ -6,26 +6,22 @@ use FastRoute\RouteCollector as FastRouteRouteCollector;
 
 class RouteCollector extends FastRouteRouteCollector {
     private $currentService;
-    private $currentServicePrefix = '';
 
     public function addRoute($httpMethod, $route, $handler, $extraParameters = []) {
         // TODO: Stop injecting extraParameters into the handler once a new FastRoute starts supporting them
+        $extraParameters['_service'] = $this -> currentService;
         parent::addRoute(
             $httpMethod,
             $route,
             [
                 'handler' => $handler,
-                'extraParameters' => [
-                    '_service' => $this -> currentService,
-                    '_servicePrefix' => $this -> currentServicePrefix
-                ] + $extraParameters
+                'extraParameters' => $extraParameters
             ]
         );
     }
 
     public function addService($service, $prefix) {
         $this -> currentService = $service;
-        $this -> currentServicePrefix = $prefix;
         $this -> addGroup($prefix, [$service, 'setupRoutes']);
     }
 }
