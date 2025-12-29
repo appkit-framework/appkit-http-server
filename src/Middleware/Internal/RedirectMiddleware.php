9b90af7023e253a2abdd3c508efd01ce857c1120
appkit-framework <admin@appkit.biz>
2025-12-29 15:01:23 +0100

    RedirectMiddleware: catch AbsoluteHttpRedirect

--- src/Middleware/Internal/RedirectMiddleware.php
+++ src/Middleware/Internal/RedirectMiddleware.php
@@ -3,14 +3,14 @@
 namespace AppKit\Http\Server\Middleware\Internal;
 
 use AppKit\Http\Middleware\HttpMiddlewareInterface;
-use AppKit\Http\Message\HttpRedirect;
+use AppKit\Http\Message\AbsoluteHttpRedirect;
 use AppKit\Http\Message\HttpResponse;
 
 class RedirectMiddleware implements HttpMiddlewareInterface {
     public function processRequest($request, $next) {
         try {
             return $next($request);
-        } catch(HttpRedirect $e) {
+        } catch(AbsoluteHttpRedirect $e) {
             $setReason = $request -> getAttribute(AccessLogMiddleware::class)['setReason'];
             $setReason('Redirect to ' . $e -> getLocation());
 
